42fe294419469d8d5b21b1117aa9533932edeffc
khazarglzd <khazarguluzade@gmail.com>
2024-06-07 01:15:13 +0400

    creating folder structure, routing & adding assets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Home,
+  Booking,
+  AboutUs,
+  Contact,
+  PageNotFound,
+  Room,
+  Services,
+  Team,
+  Testimonial,
+} from "./pages/index";
 
 
-function App() {
+export default function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <div>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/booking" element={<Booking />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/testimonial" element={<Testimonial />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/*" element={<PageNotFound />} />
+            <Route path="/rooms" element={<Room />} />
+            <Route path="/services" element={<Services />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </div>
+    </>
   );
 }
-
-export default App;
